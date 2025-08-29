1d6a21398c4687842174addfe78e834fd95c4ad2
Manisha Singh <64701224+Manisha1998-singh@users.noreply.github.com>
2025-08-30 02:38:20 +0530

    update work regarding props.

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,11 +3,11 @@ import Card from "./components/Card";
 import "./App.css";
 
 function App() {
-  let myObj = {
-    username: "manisha",
-    age: 22,
-  };
-  let myArr = [1, 4, 5, 6];
+  //   let myObj = {
+  //     username: "manisha",
+  //     age: 22,
+  //   };
+  //   let myArr = [1, 4, 5, 6];
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
