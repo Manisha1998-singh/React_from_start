8f8ab4e1c19857ea84e739eec50d9663df48efed
Manisha Singh <64701224+Manisha1998-singh@users.noreply.github.com>
2025-09-03 01:17:48 +0530

    Refactored loader function for GitHub data.

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/Manisha1998-singh")
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
+  //Commented because instead of this im using loader;
+
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/Manisha1998-singh")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     });
+  // }, []);
+  const data = useLoaderData();
   return (
     <>
       <div className="text-center m-4 bg-grey-600  p-4 text-3xl">
@@ -19,3 +23,10 @@ function Github() {
   );
 }
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch(
+    "https://api.github.com/users/Manisha1998-singh"
+  );
+  return response.json();
+};
