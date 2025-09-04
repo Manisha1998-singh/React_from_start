3a8b0005d4c315859bb0cb2151bedaa96d1560a5
Manisha Singh <64701224+Manisha1998-singh@users.noreply.github.com>
2025-09-05 02:34:50 +0530

    Implemented local storage in Todo project

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./context";
+import { TodoForm, TodoItem } from "./components";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -25,6 +26,18 @@ function App() {
       )
     );
   };
+
+  //Local storage
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
     <TodoProvider
       value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete }}>
@@ -33,9 +46,15 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoForm></TodoForm>
+          </div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem></TodoItem>
+              </div>
+            ))}
           </div>
         </div>
       </div>
