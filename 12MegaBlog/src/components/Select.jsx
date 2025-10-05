2946c5d47ab7b74bccb299e6adbe1fecb669177c
Manisha Singh <64701224+Manisha1998-singh@users.noreply.github.com>
2025-10-05 08:58:46 -1200

    Add PostCard component and update Select component

--- 12MegaBlog/src/components/Select.jsx
+++ 12MegaBlog/src/components/Select.jsx
@@ -5,13 +5,21 @@ function Select({ options, label, className, ...props }, ref) {
   return (
     <div className="w-full">
       {label && <label htmlFor={id} className={` ${className}`}></label>}
-      <select {...props} id={id} ref={ref} className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
-      {/* i added directly map on the blank condition it break the application so firstly i need to check that is option is not blank */}
-      
-        {options.map(())}
+      <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
+        {/* i added directly map on the blank condition it break the application so firstly i need to check that is option is not blank */}
+
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
     </div>
   );
 }
 
-export default Select;
+export default React.forwardRef(Select);
