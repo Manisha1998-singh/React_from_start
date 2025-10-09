ae02c744751aefcf730f0e64618e40dbe457959e
Manisha Singh <64701224+Manisha1998-singh@users.noreply.github.com>
2025-10-08 19:43:55 -1200

    Add EditPost and Home components with post fetching and rendering logic.

--- 12MegaBlog/src/pages/AllPost.jsx
+++ 12MegaBlog/src/pages/AllPost.jsx
@@ -19,7 +19,7 @@ function AllPost() {
         ))} */}
         <div className="flex flex-wrap">
           {posts.map((post) => (
-            <div key={post.$id}>
+            <div key={post.$id} className="p-2 w-1">
               {" "}
               <PostCard post={post} />
             </div>
